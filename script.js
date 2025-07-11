5db4c018f1401c9a82fe3b3d37558b5a53a8de8d
navyadeeepthi-nanduri <nandurinavyadeepthi@gmail.com>
2025-07-11 13:32:16 +0530

    Add all HTML, images, and script files

---
+++ script.js
@@ -0,0 +1,71 @@
+//console.log('hello world)
+
+
+// let z=10;
+// console.log(z)
+
+// let x=30
+// console.log(x)
+
+// let y='mahadev'// for the variable we can assign any data types 
+// console.log(y)
+
+// let a=22324325
+// console.log(a)
+let a=true
+console.log(a)
+
+var b=2234384
+
+var b=223438
+console.log(b)
+
+
+// let x=10
+// let y=20
+// console.log(x!=y)
+// const =20
+// const=30   ----its throws an error
+
+// git 
+
+// const x=20
+// const y=40
+
+
+// if(x!=y){
+//     console.log('if')
+
+// }
+// else{
+//     console.log('else')
+// }
+
+
+
+
+// const day='monday'
+// switch (day) {
+//     case "monday":
+//         console.log('week start')
+//     break;
+//     case "wednesday":
+//         console.log('mid week')
+//     break;
+//     default:
+//         console.log('end week')
+// }
+
+
+
+const day='monday'
+if(day =='monday'){
+    console.log('first week day')
+
+}
+else if(day=='wednesday'){
+    console.log('mid week')
+}
+else{
+    console.log('last week')
+}
